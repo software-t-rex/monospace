392978edbe9ae2ecbc97cd6d32f4efbcc57f0c89
malko <jgotti@jgotti.org>
2023-03-18 02:55:34 +0100

    🔖 v0.0.10

--- apps/monospace/app/consts.go
+++ apps/monospace/app/consts.go
@@ -1,6 +1,6 @@
 package app
 
-const Version string = "0.0.9"
+const Version string = "0.0.10"
 const DfltcfgFilePath string = ".monospace/monospace.yml"
 const dfltJSPM string = "^pnpm@7.27.0"
 const dfltGoModPrfx string = "example.com"
