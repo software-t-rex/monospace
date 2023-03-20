7dd40215fd14a4a3ff4ee4902eea2545378f10eb
malko <jgotti@jgotti.org>
2023-03-20 01:46:44 +0100

    🚧 wip: Introduce goreleaser

--- apps/monospace/app/consts.go
+++ apps/monospace/app/consts.go
@@ -1,6 +1,7 @@
 package app
 
-const Version string = "0.0.10"
 const DfltcfgFilePath string = ".monospace/monospace.yml"
 const dfltJSPM string = "^pnpm@7.27.0"
 const dfltGoModPrfx string = "example.com"
+
+var Version string = "next"
