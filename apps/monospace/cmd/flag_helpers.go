a271fc53557ef165b33bd557d5c21868089aa0d8
Jonathan Gotti <jonathan.gotti@gmail.com>
2023-03-24 09:18:24 +0100

    🧑‍💻 Add common project filter method

--- apps/monospace/cmd/flag_helpers.go
+++ apps/monospace/cmd/flag_helpers.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/software-t-rex/monospace/app"
 	"github.com/software-t-rex/monospace/utils"
 	"github.com/spf13/cobra"
 )
@@ -23,20 +24,57 @@ func AddFlagProjectFilter(cmd *cobra.Command) {
 	cmd.Flags().StringSliceP("project-filter", "p", []string{}, "Filter projects by name")
 	utils.CheckErr(cmd.RegisterFlagCompletionFunc("project-filter", completeProjectFilter))
 }
+func AddPersistentFlagProjectFilter(cmd *cobra.Command) {
+	cmd.PersistentFlags().StringSliceP("project-filter", "p", []string{}, "Filter projects by name")
+	utils.CheckErr(cmd.RegisterFlagCompletionFunc("project-filter", completeProjectFilter))
+}
 func completeProjectFilter(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 	suggestions := append(utils.ProjectsGetAllNameOnly(), utils.ProjectsGetAliasesNameOnly()...)
 	return suggestions, cobra.ShellCompDirectiveDefault
 }
+func GetFilterProjects(cmd *cobra.Command, repoOnly bool) []utils.Project {
+	config := utils.CheckErrOrReturn(app.ConfigGet())
+	projects := utils.ProjectsGetAll()
+	filter := utils.CheckErrOrReturn(cmd.Flags().GetStringSlice("project-filter"))
+	filterLen := len(filter)
+	if filterLen < 1 && !repoOnly { // no filter return all projects
+		return projects
+	}
+	// handle project aliases
+	if len(config.Aliases) > 0 {
+		for i, f := range filter {
+			alias := config.Aliases[f]
+			if alias != "" {
+				filter[i] = alias
+			}
+		}
+	}
+	filteredProjects := projects[:0]
+	for _, p := range projects {
+		if repoOnly && p.Kind == utils.Internal {
+			continue
+		} else if filterLen > 0 && !utils.SliceContains(filter, p.Name) {
+			continue
+		}
+		filteredProjects = append(filteredProjects, p)
+	}
+	return filteredProjects
+}
 
 // you should call ValidateFlagOutputMode in the Run of the associated command
 func AddFlagOutputMode(cmd *cobra.Command) {
-	cmd.Flags().StringP("output-mode", "O", "grouped", "output mode for multiple commands, grouped, interleaved, none")
+	cmd.Flags().StringP("output-mode", "O", "grouped", "output mode for multiple commands:\n- "+strings.Replace(outputModes, ",", "\n- ", -1)+"\n")
+	utils.CheckErr(cmd.RegisterFlagCompletionFunc("output-mode", CompleteOutputMode))
+}
+
+// you should call ValidateFlagOutputMode in the Run of the associated command
+func AddPersistentFlagOutputMode(cmd *cobra.Command) {
+	cmd.PersistentFlags().StringP("output-mode", "O", "grouped", "output mode for multiple commands:\n- "+strings.Replace(outputModes, ",", "\n- ", -1)+"\n")
 	utils.CheckErr(cmd.RegisterFlagCompletionFunc("output-mode", CompleteOutputMode))
 }
 func CompleteOutputMode(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 	return strings.Split(outputModes, ","), cobra.ShellCompDirectiveDefault
 }
-
 func ValidateFlagOutputMode(cmd *cobra.Command) string {
 	outputMode := utils.CheckErrOrReturn(cmd.Flags().GetString("output-mode"))
 	modes := strings.Split(outputModes, ",")
