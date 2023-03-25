1f101dfaf950bf6506e6993e86f875f82c45b3d5
malko <jgotti@jgotti.org>
2023-03-25 17:33:22 +0100

    ✅ Add e2e tests for ls, rename, remove commands

--- apps/monospace/cmd/e2e_test.go
+++ apps/monospace/cmd/e2e_test.go
@@ -6,7 +6,6 @@ import (
 	"path/filepath"
 	"regexp"
 	"runtime"
-	"strings"
 	"testing"
 
 	"gotest.tools/v3/assert"
@@ -31,17 +30,12 @@ func runStep(t *testing.T, name string, fn func(t *testing.T)) {
 
 func TestCmd_Suite(t *testing.T) {
 	// SETUP TEST SUITE
-	type testPathCase struct {
-		name    string
-		args    []string
-		want    icmd.Expected
-		pathOps fs.PathOp
-	}
-	type testMatchCase struct {
-		name  string
-		args  []string
-		want  icmd.Expected
-		match string
+	type testCase struct {
+		name    string        // test name
+		args    []string      // monospace args
+		want    icmd.Expected // test command output
+		pathOps fs.PathOp     // check directory structure agains pathOps
+		match   string        // regexp to run against combined output empty string is ignored
 	}
 
 	// prepare binary to test
@@ -71,9 +65,9 @@ func TestCmd_Suite(t *testing.T) {
 		}
 		return fs.WithDir(name, ops...)
 	}
-	runPathCase := func(path string) func(*testing.T, []testPathCase) {
+	runTestCases := func(path string) func(*testing.T, []testCase) {
 		dirOp := icmd.Dir(path)
-		return func(t *testing.T, tests []testPathCase) {
+		return func(t *testing.T, tests []testCase) {
 			t.Helper()
 			for _, tc := range tests {
 				t.Run(tc.name, func(t *testing.T) {
@@ -81,6 +75,10 @@ func TestCmd_Suite(t *testing.T) {
 					result.Assert(t, tc.want)
 					if tc.pathOps != nil {
 						assert.Assert(t, fs.Equal(path, fs.Expected(t, tc.pathOps, fs.MatchExtraFiles, fs.MatchAnyFileMode)))
+					}
+					if tc.match != "" {
+						exp := regexp.MustCompile(tc.match)
+						assert.Assert(t, exp.MatchString(result.Combined()), "expected to match agains %s, got: %s", tc.match, result.Combined())
 					}
 				})
 			}
@@ -117,12 +115,12 @@ func TestCmd_Suite(t *testing.T) {
 	})
 
 	runStep(t, "create", func(t *testing.T) {
-		tests := []testPathCase{
-			{"ls without project should display a message", []string{"ls"}, icmd.Expected{ExitCode: 0, Out: "No projects found start by adding one to your monospace."}, nil},
-			{"with no args", []string{"create"}, icmd.Expected{ExitCode: 1}, nil},
-			{"with 1 args", []string{"create", "local"}, icmd.Expected{ExitCode: 1}, nil},
-			{"with 2 args but invalid kind arg", []string{"create", "invalid", "apps/myapp"}, icmd.Expected{ExitCode: 1}, nil},
-			{"with invalid name", []string{"create", "internal", "apps!#/1myapp"}, icmd.Expected{ExitCode: 1}, nil},
+		tests := []testCase{
+			{"ls without project should display a message", []string{"ls"}, icmd.Expected{ExitCode: 0, Out: "No projects found start by adding one to your monospace."}, nil, ""},
+			{"with no args", []string{"create"}, icmd.Expected{ExitCode: 1}, nil, ""},
+			{"with 1 args", []string{"create", "local"}, icmd.Expected{ExitCode: 1}, nil, ""},
+			{"with 2 args but invalid kind arg", []string{"create", "invalid", "apps/myapp"}, icmd.Expected{ExitCode: 1}, nil, ""},
+			{"with invalid name", []string{"create", "internal", "apps!#/1myapp"}, icmd.Expected{ExitCode: 1}, nil, ""},
 			{"local", []string{"create", "local", "apps/myapp"}, icmd.Expected{ExitCode: 0},
 				hasDir("apps", false,
 					hasDir("myapp", false,
@@ -130,9 +128,10 @@ func TestCmd_Suite(t *testing.T) {
 						hasFile(".gitignore"),
 					),
 				),
+				"",
 			},
 			{"internal", []string{"create", "internal", "packages/mylib"}, icmd.Expected{ExitCode: 0},
-				hasDir("packages", false, hasDir("mylib", false, hasFile(".gitignore"))),
+				hasDir("packages", false, hasDir("mylib", false, hasFile(".gitignore"))), "",
 			},
 			{"internal go type", []string{"create", "internal", "packages/golib", "-t", "go"}, icmd.Expected{ExitCode: 0},
 				hasDir("packages", true, hasDir("golib", false,
@@ -140,6 +139,7 @@ func TestCmd_Suite(t *testing.T) {
 					hasFile("go.mod"),
 					hasFile("main.go"),
 				)),
+				"",
 			},
 			{"internal js type", []string{"create", "internal", "packages/jslib", "-t", "js"}, icmd.Expected{ExitCode: 0},
 				hasDir("packages", true, hasDir("jslib", false,
@@ -147,59 +147,53 @@ func TestCmd_Suite(t *testing.T) {
 					hasFile("package.json"),
 					hasFile("index.js"),
 				)),
+				"",
 			},
-			{"ls should list added projects sorted", []string{"ls", "-C"}, icmd.Expected{ExitCode: 0, Out: "apps/myapp\npackages/golib\npackages/jslib\npackages/mylib"}, nil},
+			{"ls should list added projects sorted", []string{"ls", "-C"}, icmd.Expected{ExitCode: 0, Out: "apps/myapp\npackages/golib\npackages/jslib\npackages/mylib"}, nil, ""},
 		}
 
 		t.Log("create")
-		runPathCase(initDir.Path())(t, tests)
+		runTestCases(initDir.Path())(t, tests)
 	})
 
 	runStep(t, "aliases", func(t *testing.T) {
-		tests := []testMatchCase{
-			{"with no args and no aliases return message no aliases", []string{"aliases"}, icmd.Expected{ExitCode: 0, Out: "No aliases defined\n"}, ""},
-			{"create with invalid project should error", []string{"aliases", "add", "invalidPath", "invalidAlias", "-C"}, icmd.Expected{ExitCode: 1, Err: "Error: unknown project invalidPath"}, ""},
-			{"create with invalid alias should error", []string{"aliases", "add", "packages/golib", "1golib", "-C"}, icmd.Expected{ExitCode: 1, Err: "Error: invalid alias name 1golib"}, ""},
-			{"create with valid args should work", []string{"aliases", "add", "packages/golib", "golib"}, icmd.Expected{ExitCode: 0}, ""},
-			{"create with valid args should work", []string{"aliases", "add", "packages/jslib", "jslib"}, icmd.Expected{ExitCode: 0}, ""},
-			{"list should display existing aliases", []string{"aliases", "list"}, icmd.Expected{ExitCode: 0}, "^(golib: packages/golib\n|jslib: packages/jslib\n){2}$"},
-			{"remove should fail silently on invalid alias", []string{"aliases", "remove", "test"}, icmd.Expected{ExitCode: 0}, ""},
-			{"remove should remove with valid alias", []string{"aliases", "remove", "golib"}, icmd.Expected{ExitCode: 0}, ""},
-			{"list should not display removed aliases", []string{"aliases", "list"}, icmd.Expected{ExitCode: 0}, "^jslib: packages/jslib\n$"},
+		// t.Skip()
+		tests := []testCase{
+			{"with no args and no aliases return message no aliases", []string{"aliases"}, icmd.Expected{ExitCode: 0, Out: "No aliases defined\n"}, nil, ""},
+			{"create with invalid project should error", []string{"aliases", "add", "invalidPath", "invalidAlias", "-C"}, icmd.Expected{ExitCode: 1, Err: "Error: unknown project invalidPath"}, nil, ""},
+			{"create with invalid alias should error", []string{"aliases", "add", "packages/golib", "1golib", "-C"}, icmd.Expected{ExitCode: 1, Err: "Error: invalid alias name 1golib"}, nil, ""},
+			{"create with valid args should work", []string{"aliases", "add", "packages/golib", "golib"}, icmd.Expected{ExitCode: 0}, nil, ""},
+			{"create with valid args should work", []string{"aliases", "add", "packages/jslib", "jslib"}, icmd.Expected{ExitCode: 0}, nil, ""},
+			{"list should display existing aliases", []string{"aliases", "list"}, icmd.Expected{ExitCode: 0}, nil, "^(golib: packages/golib\n|jslib: packages/jslib\n){2}$"},
+			{"remove should fail silently on invalid alias", []string{"aliases", "remove", "test"}, icmd.Expected{ExitCode: 0}, nil, ""},
+			{"remove should remove with valid alias", []string{"aliases", "remove", "golib"}, icmd.Expected{ExitCode: 0}, nil, ""},
+			{"list should not display removed aliases", []string{"aliases", "list"}, icmd.Expected{ExitCode: 0}, nil, "^jslib: packages/jslib\n$"},
 		}
 		t.Log("aliases")
-		for _, tc := range tests {
-			t.Run(tc.name, func(t *testing.T) {
-				t.Log("case:", tc.name)
-				result := runMonospace(tc.args, initDirOp)
-				result.Assert(t, tc.want)
-				if tc.match != "" {
-					exp := regexp.MustCompile(tc.match)
-					assert.Assert(t, exp.MatchString(result.Combined()))
-				}
-			})
-		}
+		runTestCases(initDir.Path())(t, tests)
 	})
 
 	runStep(t, "import", func(t *testing.T) {
-		tests := []testPathCase{
+		// t.Skip()
+		tests := []testCase{
 			{"from other repo should work", []string{"import", "modules/external", "file://" + filepath.Join(monospaceDir, "../../gomodules/js-packagemanager")}, icmd.Expected{ExitCode: 0},
 				hasDir("modules", false, hasDir("external", true,
 					hasFile("go.mod"),
-				)),
+				)), "",
 			},
-			{"repo should appear when ls", []string{"ls", "-C"}, icmd.Expected{ExitCode: 0, Out: "apps/myapp\nmodules/external\npackages/golib\npackages/jslib\npackages/mylib"}, nil},
+			{"repo should appear when ls", []string{"ls", "-C"}, icmd.Expected{ExitCode: 0, Out: "apps/myapp\nmodules/external\npackages/golib\npackages/jslib\npackages/mylib"}, nil, ""},
 		}
 		t.Log("import")
-		runPathCase(initDir.Path())(t, tests)
+		runTestCases(initDir.Path())(t, tests)
 	})
 
 	runStep(t, "clone", func(t *testing.T) {
+		// t.Skip()
 		// we need to comit changes to the initialized repo if we want something to clone
 		icmd.RunCmd(icmd.Command("git", "add", "."), initDirOp).Assert(t, icmd.Expected{ExitCode: 0})
 		icmd.RunCmd(icmd.Command("git", "commit", "-m", "commitChanges"), initDirOp).Assert(t, icmd.Expected{ExitCode: 0, Out: ""})
 
-		tests := []testPathCase{
+		tests := []testCase{
 			{"should clone external projects too", []string{"clone", initDir.Path(), "clonedRepo"}, icmd.Expected{ExitCode: 0},
 				hasDir("clonedRepo", true,
 					hasDir(".monospace", true),
@@ -215,25 +209,75 @@ func TestCmd_Suite(t *testing.T) {
 							hasFile("packagemanager.go"),
 						),
 					),
-				),
+				), "",
 			},
 			{"should error on non monospace repo", []string{"clone", "git@github.com:malko/rocketchat-jira-hook.git"}, icmd.Expected{ExitCode: 1},
-				hasDir("rocketchat-jira-hook", true, hasDir(".git", true)),
+				hasDir("rocketchat-jira-hook", true, hasDir(".git", true)), "&& monospace init",
 			},
 		}
 		t.Log("clone")
-		t.Run(tests[0].name, func(t *testing.T) {
-			test := tests[0]
-			result := runMonospace(test.args, icmd.Dir(cloneDir.Path()))
-			result.Assert(t, test.want)
-			assert.Assert(t, fs.Equal(cloneDir.Path(), fs.Expected(t, test.pathOps, fs.MatchExtraFiles, fs.MatchAnyFileMode)))
-		})
-		t.Run(tests[1].name, func(t *testing.T) {
-			test := tests[1]
-			result := runMonospace(test.args, icmd.Dir(cloneDir.Path()))
-			result.Assert(t, test.want)
-			assert.Assert(t, fs.Equal(cloneDir.Path(), fs.Expected(t, test.pathOps, fs.MatchExtraFiles, fs.MatchAnyFileMode)))
-			assert.Assert(t, strings.Contains(result.Combined(), "&& monospace init"))
-		})
+		runTestCases(cloneDir.Path())(t, tests)
+	})
+
+	runStep(t, "rename", func(t *testing.T) {
+		// t.Skip()
+		tests := []testCase{
+			{"should error on invalid project", []string{"rename", "inexsiting", "renamed"}, icmd.Expected{ExitCode: 1}, nil, "Unkwown project"},
+			{"should error with invalid project name", []string{"rename", "modules/external", "modules/&renamed"}, icmd.Expected{ExitCode: 1}, nil, "not a valid project name"},
+			{"should error if newname point to existing project", []string{"rename", "modules/external", "packages/golib"}, icmd.Expected{ExitCode: 1}, nil, "already exists"},
+			{"should error when renaming to the same name", []string{"rename", "modules/external", "modules/external"}, icmd.Expected{ExitCode: 1}, nil, "already exists"},
+			{"should work with valid args", []string{"rename", "modules/external", "modules/renamed"}, icmd.Expected{ExitCode: 0},
+				hasDir("modules", false, hasDir("renamed", true)), "",
+			},
+		}
+		t.Log("rename")
+		runTestCases(initDir.Path())(t, tests)
+	})
+
+	runStep(t, "remove", func(t *testing.T) {
+		// t.Skip()
+		tests := []testCase{
+			{"should error on unknown project", []string{"remove", "unknown"}, icmd.Expected{ExitCode: 1}, nil, "Unknown project"},
+			{"should keep the directory if -y", []string{"remove", "packages/mylib", "-y"}, icmd.Expected{ExitCode: 0},
+				hasDir("packages", false,
+					hasDir("golib", true),
+					hasDir("jslib", true),
+					hasDir("mylib", true),
+				), "",
+			},
+			{"should delete the directory if -rmdir", []string{"remove", "packages/golib", "--rmdir"}, icmd.Expected{ExitCode: 0},
+				hasDir("packages", false,
+					hasDir("jslib", true),
+					hasDir("mylib", true),
+				), "",
+			},
+		}
+		t.Log("rename")
+		runTestCases(initDir.Path())(t, tests)
+	})
+
+	runStep(t, "ls", func(t *testing.T) {
+		// t.Skip()
+		longOutput := "apps/myapp (local)\npackages/jslib (internal)"
+		tests := []testCase{
+			{"should return the list of projects", []string{"ls", "-C"}, icmd.Expected{ExitCode: 0, Out: "apps/myapp\npackages/jslib"}, nil, ""},
+			{"should add detail with -long flag", []string{"ls", "-C", "--long"}, icmd.Expected{ExitCode: 0, Out: longOutput}, nil, ""},
+			{"should support list alias", []string{"list", "-C", "-l"}, icmd.Expected{ExitCode: 0, Out: longOutput}, nil, ""},
+		}
+		t.Log("ls")
+		runTestCases(initDir.Path())(t, tests)
+	})
+
+	runStep(t, "externalize", func(t *testing.T) {
+
+	})
+	runStep(t, "exec", func(t *testing.T) {
+
+	})
+	runStep(t, "run", func(t *testing.T) {
+
+	})
+	runStep(t, "status", func(t *testing.T) {
+
 	})
 }
