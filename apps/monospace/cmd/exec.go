6e61ef3bb38edbc98c0a9d2cf1a4e7966e558eab
malko <jgotti@jgotti.org>
2023-11-01 16:07:41 +0100

    ✨ (cmd:exec) Add --git, --local, --internal, --external filter flags

--- apps/monospace/cmd/exec.go
+++ apps/monospace/cmd/exec.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/software-t-rex/monospace/app"
 	"github.com/software-t-rex/monospace/gomodules/colors"
+	"github.com/software-t-rex/monospace/mono"
 	"github.com/software-t-rex/monospace/tasks"
 	"github.com/software-t-rex/monospace/utils"
 	"github.com/spf13/cobra"
@@ -31,7 +32,11 @@ var execCmd = &cobra.Command{
 You can restrict the command to one or more projects using flag --project-filter.`,
 	Example: `  monospace exec --project-filter modules/mymodule --project-filter modules/myothermodule -- ls -la
   # or more concise
-  monospace exec -p modules/mymodule,modules/myothermodule -- ls -la`,
+  monospace exec -p modules/mymodule,modules/myothermodule -- ls -la
+	# create a branch on all git projects at once (including root)
+	monospace exec --git -r -- git checkout -b my-new-branch
+	# fetching only external projects
+	monospace exec --external -- git fetch`,
 	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		CheckConfigFound(true)
@@ -40,6 +45,12 @@ You can restrict the command to one or more projects using flag --project-filter
 		cmdBin := args[0]
 		cmdArgs := args[1:]
 		outputMode := FlagGetOutputMode(cmd, config.PreferredOutputMode)
+		filterGitOnly := FlagGetBool(cmd, "git")
+		filterExternal := FlagGetBool(cmd, "external")
+		filterInternal := FlagGetBool(cmd, "internal")
+		filterLocal := FlagGetBool(cmd, "local")
+		includeRoot := FlagGetBool(cmd, "include-root")
+		hasKindFilter := filterExternal || filterInternal || filterLocal
 
 		if cmdBin != "" && cmdBin[0] == '.' { // make relative path relative to projects
 			cmdBin = filepath.Join(utils.CheckErrOrReturn(os.Getwd()), cmdBin)
@@ -53,6 +64,16 @@ You can restrict the command to one or more projects using flag --project-filter
 		executor := tasks.NewExecutor(outputMode)
 		for _, p := range projects {
 			project := p
+			// check project match --git/--external/--internal/--local flags
+			if filterGitOnly && !p.IsGit() {
+				continue
+			}
+			if hasKindFilter && !((filterExternal && p.Kind == mono.External) ||
+				(filterInternal && p.Kind == mono.Internal) ||
+				(filterLocal && p.Kind == mono.Local) ||
+				(includeRoot && p.Kind == mono.Root)) {
+				continue
+			}
 			os.Setenv("MONOSPACE_PROJECT_KIND", project.Kind.String())
 			os.Setenv("MONOSPACE_PROJECT_NAME", project.Name)
 			os.Setenv("MONOSPACE_PROJECT_PATH", project.Path())
@@ -73,5 +94,9 @@ func init() {
 	RootCmd.AddCommand(execCmd)
 	FlagAddProjectFilter(execCmd, false)
 	FlagAddOutputMode(execCmd)
+	execCmd.Flags().Bool("git", false, "Execute command in git projects only (root has to be include with -r)")
+	execCmd.Flags().Bool("external", false, "Execute command in all external projects")
+	execCmd.Flags().Bool("internal", false, "Execute command in all internal projects (root has to be include with -r)")
+	execCmd.Flags().Bool("local", false, "Execute command in all local projects (root has to be include with -r)")
 
 }
