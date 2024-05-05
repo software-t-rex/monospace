3ccc9e2314209458be2d30c2b0a92e590bd8ec81
malko <jgotti@jgotti.org>
2024-05-05 03:27:59 +0200

    🚚 (utils) Rename styles to misc (styles doesn't make sense anymore)

--- gomodules/utils/misc.go
+++ gomodules/utils/misc.go
@@ -5,6 +5,14 @@ import (
 	"strings"
 )
 
+// This is a onliner If
+func If[T any](b bool, t, f T) T {
+	if b {
+		return t
+	}
+	return f
+}
+
 func Indent(s string, indentation string) string {
 	return fmt.Sprintf("%s%s\n", indentation, strings.Replace(s, "\n", "\n"+indentation, -1))
 }
